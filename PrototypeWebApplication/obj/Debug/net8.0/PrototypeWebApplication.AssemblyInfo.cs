6ace77621c5211d916dc14037158237681d8437e
ncf00003 <ncf00003@mix.wvu.edu>
2024-09-16 12:26:55 -0400

    Bootstrap Work for Assignment 1 -NF

--- PrototypeWebApplication/obj/Debug/net8.0/PrototypeWebApplication.AssemblyInfo.cs
+++ PrototypeWebApplication/obj/Debug/net8.0/PrototypeWebApplication.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrototypeWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11141c890901e80571e3c43f5c2c89a65fc7a617")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b348167e73f471f998cbd5c17ee096243481f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrototypeWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrototypeWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
