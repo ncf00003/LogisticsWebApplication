4d89e64aa38ee64158676d337e4f0cdd8f87e89b
ncf00003 <ncf00003@mix.wvu.edu>
2024-10-24 15:24:33 -0400

    Basic setup

---
+++ LogisticsWebAppAPI/Controllers/NFDeliveryTrackingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticsWebAppAPI.Controllers
+{
+    public class NFDeliveryTrackingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LogisticsWebAppAPI/Controllers/NFVehicleDriverController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticsWebAppAPI.Controllers
+{
+    public class NFVehicleDriverController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
