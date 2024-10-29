4aa69b4c39139790087aca18057f73824eaa4567
ncf00003 <ncf00003@mix.wvu.edu>
2024-10-29 15:55:01 -0400

    API Fix

--- LogisticsWebAppAPI/Data/DeliveryShipment.cs
+++ LogisticsWebAppAPI/Data/DeliveryShipment.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LogisticsWebAppAPI.Data
 {
@@ -14,6 +15,7 @@ namespace LogisticsWebAppAPI.Data
 	  ,T.LastUpdated
 	  ,T.OrderDate
          */
+        [Key]
         public int ShipmentId { get; set; }
 
         public DateTime? DeliveryDate { get; set; }
